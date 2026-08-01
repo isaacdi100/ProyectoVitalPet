8eeb9eef38c59f063c32b29f80f9f0a19937bfbf
isaacdi100 <dilan.analuisa5190@utc.edu.ec>
2026-07-31 21:04:20 -0600

    isa

---
+++ src/vitalpet/VitalPet.java
@@ -0,0 +1,19 @@
+package vitalpet;
+
+/**
+ *
+ * @author HP
+ */
+public class VitalPet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("holas");
+        System.out.println("holas");
+        
+    }
+    
+}
