b9d7c67f0dccfd9a806a7878476c92b38e7f9420
lilianaayala5824-lgtm <liliana.ayala5824@utc.edu.ec>
2026-08-01 23:17:42 -0500

    lili

--- src/modelo/mascotas.java
+++ src/modelo/mascotas.java
@@ -9,5 +9,51 @@ package modelo;
  * @author DELL
  */
 public class mascotas {
+    private int id_mas;
+    private String nombre_mas;
+    private String raza_mas;
+    private String nacimiento_mas;
+    private String sexo_mas;
+
+    public int getId_mas() {
+        return id_mas;
+    }
+
+    public String getNombre_mas() {
+        return nombre_mas;
+    }
+
+    public String getRaza_mas() {
+        return raza_mas;
+    }
+
+    public String getNacimiento_mas() {
+        return nacimiento_mas;
+    }
+
+    public String getSexo_mas() {
+        return sexo_mas;
+    }
+
+    public void setId_mas(int id_mas) {
+        this.id_mas = id_mas;
+    }
+
+    public void setNombre_mas(String nombre_mas) {
+        this.nombre_mas = nombre_mas;
+    }
+
+    public void setRaza_mas(String raza_mas) {
+        this.raza_mas = raza_mas;
+    }
+
+    public void setNacimiento_mas(String nacimiento_mas) {
+        this.nacimiento_mas = nacimiento_mas;
+    }
+
+    public void setSexo_mas(String sexo_mas) {
+        this.sexo_mas = sexo_mas;
+    }
+    
     
 }
